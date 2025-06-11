50f62c74d33060710eaf4571860de6df99e02de1
Ricardo Santos <153729295+RicardoSantos02@users.noreply.github.com>
2025-06-11 09:10:46 -0300

    Atualização dos .java com os codigos

---
+++ app/src/main/java/br/edu/ifrn/hoteldemedicos/OlaSpringBoot.java
@@ -0,0 +1,14 @@
+package br.edu.ifrn.hoteldemedicos;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+
+public class OlaSpringBoot {
+@GetMapping("/")
+public String index(){
+    return "olá spring boot";
+}
+
+}
